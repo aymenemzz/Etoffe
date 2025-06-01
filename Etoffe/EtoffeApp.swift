d5be51ccfa3c31732e56070faf70910086c8f105
amazouz <mzz.aymene@gmail.com>
2025-06-01 03:48:50 +0200

    First commit

---
+++ Etoffe/EtoffeApp.swift
@@ -0,0 +1,33 @@
+//
+//  EtoffeApp.swift
+//  Etoffe
+//
+//  Created by Aymene Mazouz on 01/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct EtoffeApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ EtoffeTests/EtoffeTests.swift
@@ -0,0 +1,17 @@
+//
+//  EtoffeTests.swift
+//  EtoffeTests
+//
+//  Created by Aymene Mazouz on 01/06/2025.
+//
+
+import Testing
+@testable import Etoffe
+
+struct EtoffeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
